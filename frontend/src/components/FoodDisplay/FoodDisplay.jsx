446a84d7d18fe74915127384763be57f76a57018
ALW-N <officialalwintomy@gmail.com>
2025-02-04 12:21:13 +0530

    Commit-9

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,7 +3,7 @@ import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext';
 import FoodItem from '../FoodItem/FoodItem';
 
-const FoodDisplay = ({catgeory}) => {
+const FoodDisplay = ({category}) => {
     const {food_list} = useContext(StoreContext);
 
   return (
@@ -11,7 +11,9 @@ const FoodDisplay = ({catgeory}) => {
         <h2>Top Dishes Near You</h2>
         <div className="food-display-list">
             {food_list.map((item,index)=>{
-                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+                if (category === "All" || category === item.category) {
+                    return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+                }                
             })}
         </div>
     </div>
